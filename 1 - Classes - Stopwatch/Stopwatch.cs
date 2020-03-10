dd626beb78d9c95f18a881506ac472c23cb53789
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-10 16:40:19 -0700

    Create a Stopwatch class

---
+++ 1 - Classes - Stopwatch/Stopwatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1_Stopwatch
+{
+    class Stopwatch
+    {
+        public TimeSpan Duration { get; private set; }
+        private DateTime _startTime;
+        private bool _running;
+
+        public void Start()
+        {
+            if (_running)
+                throw new InvalidOperationException();
+
+            _startTime = DateTime.Now;
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException();
+
+            Duration = DateTime.Now - _startTime;
+            _running = false;
+        }
+
+    } // Stopwatch class
+
+} // Exercise1_Stopwatch namespace
+
