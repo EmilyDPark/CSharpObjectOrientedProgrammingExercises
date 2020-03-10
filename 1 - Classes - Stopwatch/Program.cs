76b6306f8234b3e0f640fbb5e1465e4f8516e343
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-10 16:40:06 -0700

    Implement created Stopwatch class

---
+++ 1 - Classes - Stopwatch/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1_Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            Console.WriteLine("Stopwatch has started.\nPress enter to stop.");
+            stopwatch.Start();
+            Console.ReadLine();
+
+            Console.WriteLine("Stopwatch has stopped.");
+            stopwatch.Stop();
+            Console.WriteLine($"Duration: {stopwatch.Duration}\n");
+
+
+            Console.WriteLine("Press enter to run the stopwatch again.");
+            Console.ReadLine();
+
+            Console.WriteLine("Stopwatch has started.\nPress enter to stop.");
+            stopwatch.Start();
+            Console.ReadLine();
+
+            Console.WriteLine("Stopwatch has stopped.");
+            stopwatch.Stop();
+            Console.WriteLine($"Duration: {stopwatch.Duration}\n");
+
+
+            Console.WriteLine("Press enter to run the stopwatch again.");
+            Console.ReadLine();
+
+            Console.WriteLine("Stopwatch has started.\nPress enter to stop.");
+            stopwatch.Start();
+            Console.ReadLine();
+
+            Console.WriteLine("Stopwatch has stopped.");
+            stopwatch.Stop();
+            Console.WriteLine($"Duration: {stopwatch.Duration}\n");
+
+
+            //stopwatch.Start();
+            //stopwatch.Start(); 
+
+        } // Main method
+
+    } // Program class
+
+} // Exercise1_Stopwatch namespace
