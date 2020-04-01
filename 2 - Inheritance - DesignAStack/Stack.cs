3cc02a96221cab0a3b0743c0eba9ca33b36e8e27
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-04-01 08:04:46 -0700

    Create a Stack class

---
+++ 2 - Inheritance - DesignAStack/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1_DesignAStack
+{
+    class Stack
+    {
+        private readonly List<object> stack = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException();
+
+            stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (stack.Count == 0)
+                throw new InvalidOperationException();
+
+            var pop = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            return pop;
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+    }
+}
