aa36491c4ba2da08cdd5b39c76c3941980d341f4
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-04-03 10:08:03 -0700

    Implement created classes

---
+++ 3 - Polymorphism - DesignADababaseCommand/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_DesignADatabaseCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection1 = new DbCommand("Add new column", new SqlConnection("abcde"));
+            connection1.Execute();
+
+            Console.WriteLine();
+
+            var connection2 = new DbCommand("Add new row", new OracleConnection("fghij"));
+            connection2.Execute();
+        }
+    }
+}
