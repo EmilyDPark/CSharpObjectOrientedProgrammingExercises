ca896f18a7bbd53641faeacf763714756df1ddbb
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-04-03 10:07:54 -0700

    Create database command class

---
+++ 3 - Polymorphism - DesignADababaseCommand/DbCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_DesignADatabaseCommand
+{
+    public class DbCommand
+    {
+        private string _command { get; set; }
+        private DbConnection _connection { get; set; }
+
+        public DbCommand(string command, DbConnection connection)
+        {
+            if (string.IsNullOrEmpty(command))
+                throw new InvalidOperationException("Command cannot be null or empty.");
+
+            _command = command;
+            _connection = connection;
+        }
+
+        public void Execute()
+        {
+            _connection.Open();
+            Console.WriteLine("Execute: " + _command);
+            _connection.Close();
+        }
+    }
+}
