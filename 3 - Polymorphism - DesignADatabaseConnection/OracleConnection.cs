c1eff5fbca672813d092bc8f75894ad3280dcebe
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-04-03 10:09:02 -0700

    Create an Oracle connection class

---
+++ 3 - Polymorphism - DesignADatabaseConnection/OracleConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1_DesignADatabaseConnection
+{
+    public class OracleConnection : DbConnection
+    {
+        public string _connectionString { get; set; }
+
+        public OracleConnection(string connectionString)
+            : base(connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine($"Open Oracle connection: {_connectionString}");
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine($"Close Oracle connection: {_connectionString}");
+        }
+    }
+}
